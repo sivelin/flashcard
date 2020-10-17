0b004fe1f9048148f4252b0630d9ef30d52391ca
SivElin <sivelin.odegard@students.fhnw.ch>
2020-10-17 09:25:39 +0200

    UB4 Aufgabe 1

--- src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/questionnaires")
@@ -37,5 +39,12 @@ public class QuestionnaireController {
 		Optional<Questionnaire> questionnaire = questionnaireRepository.findById(id);
 		model.addAttribute("questionnaire", questionnaire.get());
 		return "questionnaires/show";
-	}
+    }
+    
+    @PostMapping(value=".../questionnaires?form")
+    public Questionnaire postMethodName(@RequestBody Questionnaire entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
 }
